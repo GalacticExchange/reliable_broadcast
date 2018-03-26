567b6e92c31ae2d0cc94aaf99edc4b85bf002281
Bogdan <bn.bliznyuk@gmail.com>
2018-03-26 17:09:24 +0300

    ansible dir structure

---
+++ ops/ansible/rerun.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+#set -e
+#set -x
+
+POSITIONAL=()
+
+build(){
+   ./build.sh
+}
+
+
+while [[ $# -gt 0 ]]
+do
+    key="$1"
+
+    case $key in
+        -b|--build)
+        build
+        shift
+        ;;
+    esac
+done
+set -- "${POSITIONAL[@]}" # restore positional parameters
+
+ansible-playbook -i inventories/inventory playbooks/rerun_nodes.yml
